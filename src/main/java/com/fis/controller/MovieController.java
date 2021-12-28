18c6fefbcba01c557b2a07b2713b33688cacba39
samidi <samidi.java@gmail.com>
2021-12-28 22:24:02 +0530

    comments added

--- src/main/java/com/fis/controller/MovieController.java
+++ src/main/java/com/fis/controller/MovieController.java
@@ -1,6 +1,9 @@
 package com.fis.controller;
 
 import java.util.List;
+/*
+ * This is movie controller class
+ */
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.fis.model.Movie;
 import com.fis.service.MovieService;
