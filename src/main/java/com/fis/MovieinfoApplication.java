0cf15680c1c6fa8504caac92055a66c6ebd373d4
samidi <samidi.java@gmail.com>
2021-12-30 21:20:52 +0530

    Removed the cloud config

--- src/main/java/com/fis/MovieinfoApplication.java
+++ src/main/java/com/fis/MovieinfoApplication.java
@@ -2,10 +2,8 @@ package com.fis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class MovieinfoApplication {
 
 	public static void main(String[] args) {
