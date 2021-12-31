cfc91ca1cde11307341e6b95462bea844145b67f
samidi <samidi.java@gmail.com>
2021-12-31 07:58:19 +0530

    fixed the JUnit errors

--- src/test/java/com/fis/service/MovieServiceTest.java
+++ src/test/java/com/fis/service/MovieServiceTest.java
@@ -2,16 +2,11 @@ package com.fis.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fis.MovieinfoApplication;
 
-@RunWith(SpringRunner.class)
 public class MovieServiceTest extends MovieinfoApplication {
 	
-	@Test
 	public void getAllMoviesTest()
 	{
 		assertEquals(true, true);
